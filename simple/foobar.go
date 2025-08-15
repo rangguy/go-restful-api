bc3690e4708fe4b80dc45ea19505db6af911a99d
rangguy <ranggadwi100@gmail.com>
2025-08-15 13:31:04 +0700

    provider set

---
+++ simple/foobar.go
@@ -0,0 +1,10 @@
+package simple
+
+type FooBarService struct {
+	FooService *FooService
+	BarService *BarService
+}
+
+func NewFooBarService(fooService *FooService, barService *BarService) *FooBarService {
+	return &FooBarService{FooService: fooService, BarService: barService}
+}

c630089b2ee230a79c8e2fb9432f012d029bb2e0
rangguy <ranggadwi100@gmail.com>
2025-10-22 21:23:10 +0700

    database migration

--- app/database.go
+++ app/database.go
@@ -7,7 +7,8 @@ import (
 )
 
 func NewDB() *sql.DB {
-	db, err := sql.Open("mysql", "root@tcp(127.0.0.1:3306)/belajar_golang_restful_api")
+	//db, err := sql.Open("mysql", "root@tcp(127.0.0.1:3306)/belajar_golang_restful_api")
+	db, err := sql.Open("mysql", "root@tcp(127.0.0.1:3306)/belajar_golang_database_migration")
 	helper.PanicIfError(err)
 
 	db.SetMaxIdleConns(5)
@@ -16,4 +17,17 @@ func NewDB() *sql.DB {
 	db.SetConnMaxIdleTime(10 * time.Minute)
 
 	return db
+
+	//migrate create -ext sql -dir db/migrations create_table_first
+	//migrate create -ext sql -dir db/migrations create_table_second
+	//migrate create -ext sql -dir db/migrations create_table_third
+
+	// migrate -database "mysql://root@tcp(localhost:3306)/belajar_golang_database_migration" -path db/migrations up
+	// migrate -database "mysql://root@tcp(localhost:3306)/belajar_golang_database_migration" -path db/migrations down
+
+	// migrate -database "mysql://root@tcp(localhost:3306)/belajar_golang_database_migration" -path db/migrations up (angka)
+	// migrate -database "mysql://root@tcp(localhost:3306)/belajar_golang_database_migration" -path db/migrations down (angka)
+
+	// migrate -database "mysql://root@tcp(localhost:3306)/belajar_golang_database_migration" -path db/migrations version
+	// migrate -database "mysql://root@tcp(localhost:3306)/belajar_golang_database_migration" -path db/migrations force 20251022140403
 }
---
+++ db/migrations/20251022140943_create_table_wrong.up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE correct
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+)ENGINE = InnoDB;
+
+
+CREATE TABLE wrong
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+)ENGINE = InnoDB;
